85b5a58d99ba18c948f867f6f12c859396e9d2db
longsl <583297550@qq.com>
2019-02-14 10:04:39 +0800

    Kids Puzzle Game
    Game demo created and submitted

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] Images;
+    [SerializeField]
+    private GameObject winText;
+    public static bool youWin;
+
+    public Animator gameFinishPanelAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+        youWin = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Images.Length == 6 &&
+            Images[0].rotation.z == 0 &&
+            Images[1].rotation.z == 0 &&
+            Images[2].rotation.z == 0 &&
+            Images[3].rotation.z == 0 &&
+            Images[4].rotation.z == 0 &&
+            Images[5].rotation.z == 0)
+        {
+            youWin = true;
+            winText.SetActive(true);
+            StartCoroutine(GameFinishPanel());
+        }
+        else if(Images.Length == 8 &&
+            Images[0].rotation.z == 0 &&
+            Images[1].rotation.z == 0 &&
+            Images[2].rotation.z == 0 &&
+            Images[3].rotation.z == 0 &&
+            Images[4].rotation.z == 0 &&
+            Images[5].rotation.z == 0 &&
+            Images[6].rotation.z == 0 &&
+            Images[7].rotation.z == 0)
+        {
+            youWin = true;
+            winText.SetActive(true);
+            StartCoroutine(GameFinishPanel());
+        }
+        
+    }
+
+    IEnumerator GameFinishPanel()
+    {
+        yield return new WaitForSeconds(1f);
+        gameFinishPanelAnim.Play("SlideIn");
+    }
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/RotateImage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateImage : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if (!GameController.youWin)
+            transform.Rotate(0f, 0f, 90f);
+    }
+}
